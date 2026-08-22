40f486e3ae8a18e65b2d151aaf51620fad611a82
mauryarajan782-design <mauryarajan782@gmail.com>
2026-08-23 01:51:09 +0530

    update

--- src/main/java/seekho/spring/main/Main.java
+++ src/main/java/seekho/spring/main/Main.java
@@ -14,10 +14,9 @@ public class Main {
 		Employee bean1 = ioc.getBean("emp1",Employee.class);
 		System.out.println(bean1);
 		
+		
 		Employee bean2 = ioc.getBean("emp2",Employee.class);
 		System.out.println(bean2);
-		
 
 	}
-
 }
